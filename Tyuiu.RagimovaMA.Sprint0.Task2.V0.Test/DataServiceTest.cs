c68da1241d240046d6d5019fb940b1729ec29958
Tyuiu.RagimovaMA@outlook.com <Tyuiu.RagimovaMA@outlook.com>
2023-09-25 22:52:46 +0500

    Task2

---
+++ Tyuiu.RagimovaMA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.RagimovaMA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.RagimovaMA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Мадина";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Мадина", res);
+        }
+    }
+}
